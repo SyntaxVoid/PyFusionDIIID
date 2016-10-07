112028818c0c705311dd493efa47ace3e7fa5c7c
SyntaxVoid <j.gresl12@gmail.com>
2016-10-07 15:42:43 -0700

    Changed test.py to produce a sample cluster for my poster

--- pyfusion/test.py
+++ pyfusion/test.py
@@ -1,7 +1,27 @@
-d = {"NAME": "John", "AGE": 21, "LIKES": ["physics","math","4"]}
+import random
+from matplotlib import pyplot as plt
+from matplotlib import ticker as mtick
 
 
-def print_dict(d):
-    f_str = "\t{{:{}}}:  {{}}".format(max(map(len, d.keys()))+2)
-    for k,v in d.items():
-        print(f_str.format(k,v))
+def make_cluster(xmin,ymin,xmax,ymax,npts):
+    xout = []
+    yout = []
+    for i in range(npts):
+        xout.append(random.uniform(xmin,xmax))
+        yout.append(random.uniform(ymin,ymax))
+    return xout, yout
+
+c1 = make_cluster(1,.2,2,2,35)
+c2 = make_cluster(3,0.3,4,2,32)
+c3 = make_cluster(1,3,3,5,37)
+
+loc = mtick.MultipleLocator(base=0.5)
+fig,ax = plt.subplots()
+ax.plot(c1[0],c1[1],"ro",c2[0],c2[1],"go",c3[0],c3[1],"bo")
+ax.xaxis.set_major_locator(loc)
+ax.yaxis.set_major_locator(loc)
+plt.axis([0,5,0,6])
+ax.grid()
+plt.title("Clustering Example")
+plt.legend(["Cluster 1","Cluster 2","Cluster 3"])
+plt.show()
