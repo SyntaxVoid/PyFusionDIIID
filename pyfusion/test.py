afda99c4434ca5030c1887a9ae109093bf576508
SyntaxVoid <j.gresl12@gmail.com>
2016-10-09 18:52:58 -0700

    debug messages

--- pyfusion/test.py
+++ pyfusion/test.py
@@ -10,10 +10,11 @@ other_array_labels = [ ]
 meta_data = ["kh", "heating_freq", "main_current","sec_current","shot"]
 ext_settings_svd = {"min_svs": 2, "power_cutoff": 0.006, "lower_freq": 4000, "upper_freq": 200000}
 
+print("Before svd multi extract.")
 svd_data = ext.multi_extract_DIIID(dataset,array,other_arrays=other_arrays,other_array_labels=other_array_labels,
-                                   meta_data=meta_data, n_cpus=8, NFFT=1024, overlap=4,
+                                   meta_data=meta_data, n_cpus=1, NFFT=1024, overlap=4,
                                    extraction_settings=ext_settings_svd,method="svd")
-
+print("After svd multi extract.")
 datamining_settings = {"n_clusters": 16, "n_iterations": 20, "start": "k_means", "verbose": 1, "method": "EM_VMM"}
 
 extraction_settings = {"n_pts": 5, "lower_freq": 1500, "filter_cutoff": 0.18, "cutoff_by": "sigma_bar",
