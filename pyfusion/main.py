c55f63d7eac0be2b8031290e6ea31910716f1b86
SyntaxVoid <j.gresl12@gmail.com>
2016-10-19 13:14:41 -0700

    Better plots hopefully

--- pyfusion/main.py
+++ pyfusion/main.py
@@ -14,10 +14,10 @@ import pyfusion.clustering as clust
 
 def run():
     verbose = True
-    shots = [159243,159244,159245,159246,159247,159248,159249,159250,159251]
+    shots = [159243]
     time_window = D3DR.DEFAULT_TIME_WINDOW
-    nrows = 3
-    ncols = 3
+    nrows = 2
+    ncols = 2
     figure, axes = plt.subplots(nrows=nrows, ncols=ncols, sharex=True, sharey=True)
     axf = axes.flatten()
     rep = itertools.repeat
