7e77cacecb2931be3cd7757a0114c132c8696ddd
John Gresl <j.gresl12@gmail.com>
2017-02-01 14:56:07 -0800

    Checking signals

---
+++ pyfusion/Signal_Check.py
@@ -0,0 +1,38 @@
+from multiprocessing import Pool
+import pyfusion as pf
+import numpy as np
+import copy, os, time, itertools
+import matplotlib.pyplot as plt
+import cPickle as pickle
+import pyfusion.clustering as clust
+import pyfusion.clustering.extract_features_scans as ext
+
+
+
+
+def run(shot,time_window):
+    dev = pf.getDevice("DIIID")
+    mag = dev.acq.getdata("DIIID_toroidal_mag")
+    mag = mag.reduce_time(time_window)
+    n_signals = len(mag.channels)
+    print("{} Signals".format(n_signals))
+    time = mag.timebase.tolist()
+
+    nrows=4
+    ncols=4
+    fig,ax=plt.subplots(nrows=nrows,ncols=ncols,sharex=True,sharey=True)
+    axf = ax.flatten()
+    plt.grid()
+    for n,cur_ax in enumerate(axf):
+        sig = mag.signal[n].tolist()
+        name = mag.channels[n].name
+        im = cur_ax.plot(time,sig)
+        cur_ax.text(3050,400,name,fontsize=20)
+    fig.canvas.draw()
+    fig.show()
+
+
+if __name__ == '__main__':
+
+
+    pass
