d7da6b7e3f00225e852bba59ceb87725a20bcd29
John Gresl <j.gresl12@gmail.com>
2017-02-19 15:20:30 -0800

    Pickling workaround for multiprocessing.

--- pyfusion/Analysis.py
+++ pyfusion/Analysis.py
@@ -98,7 +98,7 @@ class Analysis:
     def run_analysis(self,method="stft",savefile=None):
         if method == "stft":
             func = stft_pickle_workaround
-        tmp_data_iter = itertools.izip(itertools.repeat(self),self.input_data_iter)
+        tmp_data_iter = itertools.izip(itertools.repeat(self),itertools.izip(self.shots,self.time_windows))
         if self.n_cpus > 1:
             pool = Pool(processes = self.n_cpus, maxtasksperchild=3)
             self.results = pool.map(func, tmp_data_iter)
