40791828421b189f9c2bc159a3fa22fecf382c5d
SyntaxVoid <j.gresl12@gmail.com>
2017-02-28 16:04:45 -0800

    Fixed Typo.

--- pyfusion/Analysis.py
+++ pyfusion/Analysis.py
@@ -219,7 +219,7 @@ class Analysis:
             plt.figure(1)
             plt.xlim(self.time_windows[0])
             plt.ylim([0,250])
-            x0 = self.time_windows[0] + 0.35 * (sum(self.time_windows[0]))
+            x0 = self.time_windows[0][0] + 0.35 * (sum(self.time_windows[0]))
             y0 = 0.9*250
             plt.text(x0, y0, shot, bbox=dict(facecolor="red", alpha=0.75), fontsize=24)
             plt.figure(2)
