8bf882289ef5b8d3a04be6fa584123ceecf37cae
SyntaxVoid <j.gresl12@gmail.com>
2017-03-12 16:23:59 -0700

    pretty graphs incoming.

--- pyfusion/ByHand.py
+++ pyfusion/ByHand.py
@@ -58,6 +58,7 @@ def plot_diagnostics(A, time_window, t0, f0):
 
     plt.figure(num=None, figsize=(11,8.5), dpi=100, facecolor="w", edgecolor="k")
     plt.rc("text", usetex=True)
+    plt.rc("text.latex", unicode=True)
     ax1 = plt.subplot2grid((2, 3), (0, 0))
     ax2 = plt.subplot2grid((2, 3), (1, 0))
     ax3 = plt.subplot2grid((2, 3), (0, 1), rowspan=2, colspan=2)
