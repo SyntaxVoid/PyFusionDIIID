d394888971e06b954a8cb248bacf9ed9bcfaf31a
SyntaxVoid <j.gresl12@gmail.com>
2017-03-12 16:03:16 -0700

    pretty graphs incoming.

--- pyfusion/ByHand.py
+++ pyfusion/ByHand.py
@@ -71,8 +71,8 @@ def plot_diagnostics(A, time_window, t0, f0):
     ax2.set_xlim([0, 360])
     ax2.set_xticks(np.arange(0, 360 + 1, 60))
     ax2.set_yticks([-pi, -3*pi/4, -pi/2, -pi/4, 0, pi/4, pi/2, 3*pi/4, pi])
-    #ax2.set_yticklabelss(["$-\pi$", r"$-\frac{3\pi}{4}$", r"$-\frac{\pi}{2}$", r"$-\frac{\pi}{4}$", "$0$",
-    #                     r"$\frac{\pi}{2}$", r"$\pi$", r"$\frac{3\pi}{2}$", r"$2\pi$"])
+    ax2.set_yticklabels(["$-\pi$", r"$-\frac{3\pi}{4}$", r"$-\frac{\pi}{2}$", r"$-\frac{\pi}{4}$", "$0$",
+                         r"$\frac{\pi}{2}$", r"$\pi$", r"$\frac{3\pi}{2}$", r"$2\pi$"])
     
     ax2.grid()
 
