60a0f8e578eff7453ecc0b4ae46a0c6d46cf4733
SyntaxVoid <j.gresl12@gmail.com>
2017-03-12 22:49:56 -0700

    .

--- pyfusion/ByHand.py
+++ pyfusion/ByHand.py
@@ -21,9 +21,9 @@ def plot_clusters(A, clust_arr, ax=None):
         def __getitem__(self, key):
             return self.arr[np.mod(key, len(self.arr))]
 
-    plot_colors = CycledList(["silvero", "redo", "siennao", "goldo", "olivedrabo", "chartreuseo",
-                              "mediumspringgreeno", "lightseagreeno", "darkcyano", "co", "deepskyblueo",
-                              "royalblueo", "darkorchido", "mo", "mediumvioletredo", "lightsalmono"])
+    plot_colors = CycledList(["silver", "red", "sienna", "gold", "olivedrab", "chartreuse",
+                              "mediumspringgreen", "lightseagreen", "darkcyan", "c", "deepskyblue",
+                              "royalblue", "darkorchid", "m", "mediumvioletred", "lightsalmon"])
     if ax is None:
         plt.specgram(A.results[0][2][0, :], NFFT=1024, Fs=1./np.mean(np.diff(A.results[0][3])),
                      noverlap=128, xextent=[A.results[0][3][0], A.results[0][3][-1]])
@@ -31,7 +31,9 @@ def plot_clusters(A, clust_arr, ax=None):
             mask = (A.z.cluster_assignments==cl)
             plt.plot(A.z.feature_obj.misc_data_dict["time"][mask],
                      A.z.feature_obj.misc_data_dict["freq"][mask],
-                     plot_colors[cl], markersize=A.markersize)
+                     color=plot_colors[cl], marker="o",
+                     markersize=A.markersize)
+
     else:
         ax.specgram(A.results[0][2][0, :], NFFT=1024, Fs=1. / np.mean(np.diff(A.results[0][3])),
                      noverlap=128, xextent=[A.results[0][3][0], A.results[0][3][-1]])
@@ -39,7 +41,8 @@ def plot_clusters(A, clust_arr, ax=None):
             mask = (A.z.cluster_assignments == cl)
             ax.plot(A.z.feature_obj.misc_data_dict["time"][mask],
                      A.z.feature_obj.misc_data_dict["freq"][mask],
-                     plot_colors[cl], markersize=A.markersize)
+                     color=plot_colors[cl], marker="o",
+                     markersize=A.markersize)
     return
 
 
