f7eb850337520dbe6a6744810f33200234002392
SyntaxVoid <j.gresl12@gmail.com>
2017-03-12 17:13:29 -0700

    pretty graphs incoming.

--- pyfusion/ByHand.py
+++ pyfusion/ByHand.py
@@ -102,8 +102,8 @@ def plot_diagnostics(A, time_window, t0, f0):
     ax3.set_xlim(time_window)
     ax3.set_ylim([45, 250])
 
-    tmp = "Shot 159243 at t = {} ms, f = {} kHz".format(t,f)
-    plt.suptitle(r"\mathrm{{}}".format(tmp), fontsize=24)
+    tmp = "/mathrm{{Shot 159243 at t = {} ms, f = {} kHz}}".format(t,f)
+    plt.suptitle(tmp, fontsize=24)
     #plt.subplot_tool()
     #file_format_str = "../Plots/Shot{}_Time{}_Freq{}.png"
     #plt.savefig(file_format_str.format(A.shots[0],t0,f0))
