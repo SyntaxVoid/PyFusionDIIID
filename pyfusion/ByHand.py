c9716700f897210a2905a3c85d34d52703a19544
SyntaxVoid <j.gresl12@gmail.com>
2017-03-12 22:55:41 -0700

    .

--- pyfusion/ByHand.py
+++ pyfusion/ByHand.py
@@ -21,9 +21,9 @@ def plot_clusters(A, clust_arr, ax=None):
         def __getitem__(self, key):
             return self.arr[np.mod(key, len(self.arr))]
 
-    plot_colors = CycledList(["silver", "red", "sienna", "gold", "olivedrab", "chartreuse",
-                              "mediumspringgreen", "lightseagreen", "darkcyan", "c", "deepskyblue",
-                              "royalblue", "darkorchid", "m", "mediumvioletred", "lightsalmon"])
+    plot_colors = CycledList(["silver", "darkorchid", "chartreuse", "c", "sienna", "gold", "olivedrab",
+                              "mediumspringgreen", "lightseagreen", "darkcyan", "deepskyblue",
+                              "royalblue","red", "m", "mediumvioletred", "lightsalmon"])
     if ax is None:
         plt.specgram(A.results[0][2][0, :], NFFT=1024, Fs=1./np.mean(np.diff(A.results[0][3])),
                      noverlap=128, xextent=[A.results[0][3][0], A.results[0][3][-1]])
